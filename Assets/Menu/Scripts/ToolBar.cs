ff33e21327147305b7870735258c5e006e75d8dd
Tae In Shim <80461160+Nyazure@users.noreply.github.com>
2023-12-03 17:57:44 -0600

    help
    help

--- Assets/Menu/Scripts/ToolBar.cs
+++ Assets/Menu/Scripts/ToolBar.cs
@@ -8,17 +8,19 @@ public class ToolBar : MonoBehaviour
 
     public GameObject selected_tower = null;
 
-    public int tower1_price;
+    public int tower1_price = 10;
     GameObject tower1;
 
-    public int tower2_price;
+    public int tower2_price = 15;
     GameObject tower2;
 
+    Player p;
     // Start is called before the first frame update
     void Start()
     {
         tower1 = FindObjectOfType<BasicTower>().gameObject;
         tower2 = FindObjectOfType<IceTower>().gameObject;
+        p = FindObjectOfType<Player>();
     }
 
     public void Buy_Tower1()
@@ -33,7 +35,7 @@ public class ToolBar : MonoBehaviour
 
     private void Buy(GameObject tower, int price)
     {
-        Player p = FindObjectOfType<Player>();
+        
         if (p.money >= price)
         {
             p.money -= price;

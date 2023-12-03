1da0ca407b6faea58bae859b0b6258a5cbf1043e
hcordes02 <145809715+hcordes02@users.noreply.github.com>
2023-12-02 20:39:29 -0600

    menu

---
+++ Assets/Menu/PauseMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ToMainMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Menu/ToolBar.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolBar : MonoBehaviour
+{
+    public int tower1 = 10;
+    public int tower2 = 15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Buy_Tower1()
+    {
+        BuyItem(tower1);
+    }
+
+    public void Buy_Tower2()
+    {
+        BuyItem(tower2);
+    }
+
+    private void BuyItem(int price)
+    {
+        Player p = FindObjectOfType<Player>();
+        if (p.money >= price)
+        {
+            p.money -= price;
+        }
+    }
+}
